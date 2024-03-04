62fb7f925fee1aa9d54fc86f249908d8956d40d1
Rayan <rayan.benguedih@epitech.eu>
2024-03-04 12:58:22 +0100

    updated some choding style

--- shakespearianModelOrion.py
+++ shakespearianModelOrion.py
@@ -25,40 +25,40 @@ class Orion(tf.keras.Model):
             return x
 
 class singleStep(tf.keras.Model):
-    def __init__(self, model, chars_from_ids, ids_from_chars, temp=1.0):
+    def __init__(self, model, ids2chars, chars2idx, temp=1.0):
         super().__init__()
         self.temp = temp
         self.model = model
-        self.chars_from_ids = chars_from_ids
-        self.ids_from_chars = ids_from_chars
+        self.ids2chars = ids2chars
+        self.chars2idx = chars2idx
 
-        # Create a mask to prevent "" or "[UNK]" from being generated.
-        skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+        # Create a mask to prevent undesired chars in the output
+        skip_ids = self.chars2idx(['[UNK]'])[:, None]
         sparse_mask = tf.SparseTensor(
         # Put a -inf at each bad index.
             values=[-float('inf')]*len(skip_ids),
             indices=skip_ids,
         # Match the shape to the vocabulary
-            dense_shape=[len(ids_from_chars.get_vocabulary())])
-        self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+            dense_shape=[len(chars2idx.get_vocabulary())])
+        self.mask = tf.sparse.to_dense(sparse_mask)
 
 
     @tf.function
     def gen_step(self, inputs, states=None):
         input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
-        input_ids = self.ids_from_chars(input_chars).to_tensor()
+        input_ids = self.chars2idx(input_chars).to_tensor()
 
         # Run the model.
-        predicted_logits, states = self.model(inputs=input_ids, states=states, return_state=True)
+        log_pred, states = self.model(inputs=input_ids, states=states, return_state=True)
 
-        #USe last pred
-        predicted_logits = predicted_logits[:, -1, :]
-        predicted_logits = predicted_logits/self.temp
-        predicted_logits = predicted_logits + self.prediction_mask
+        #USe lastest pred
+        log_pred = log_pred[:, -1, :]
+        log_pred = log_pred/self.temp
+        log_pred = log_pred + self.mask
         #Use the mask
 
         # Sample the output logits to generate token IDs.
-        predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+        predicted_ids = tf.random.categorical(log_pred, num_samples=1)
         predicted_ids = tf.squeeze(predicted_ids, axis=-1)
 
         # Convert from token ids to characters
